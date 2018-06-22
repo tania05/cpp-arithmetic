24bf97b16d259e7a79a187b853febd20a69706cd
Tania Akter <tania9309@gmail.com>
2018-06-22 04:32:27 -0700

    Changing order and adding checcks to see only the flippable edges are being considerd

--- app/delaunay_triangulation.cpp
+++ app/delaunay_triangulation.cpp
@@ -11,16 +11,32 @@ using Kernel = CGAL::Cartesian<double>;
 using Triangulation = trilib::Triangulation_2<Kernel>;
 using Suspect_Map = std::map<Triangulation::Halfedge_const_handle, int>;
 
+Suspect_Map suspects;
+
 template <class T>
 void lop()
 {
     std::cout<< "You are here" << std::endl;
 }
 
-//find neighboring faces, return edges associated
-void mark_potential_suspects(const Triangulation& tri, Triangulation::Halfedge_const_handle flippedEdge, Suspect_Map& suspects)
+bool is_flippable(Triangulation::Halfedge_const_handle halfedge)
 {
-  std::cout << "faces in triangulation:\n";
+  return !(halfedge->is_border() || halfedge->opposite()->is_border());
+}
+
+
+void mark_edges(Triangulation::Halfedge_const_handle edge)
+{
+  if(is_flippable(edge))
+  {
+    suspects[edge] = 0;
+    suspects[edge->opposite()] = 0;
+  }
+}
+
+//find neighboring faces, return edges associated
+void mark_potential_suspects(const Triangulation& tri, Triangulation::Halfedge_const_handle flippedEdge)
+{
 	for (auto faceIter = tri.faces_begin(); faceIter != tri.faces_end();
 	  ++faceIter) {
 		Triangulation::Halfedge_const_handle h = faceIter->halfedge();
@@ -32,8 +48,10 @@ void mark_potential_suspects(const Triangulation& tri, Triangulation::Halfedge_c
 
       if(h == flippedEdge)
       {
-        suspects[h->next()] = 0;
-        suspects[h->next()->next()] = 0;
+        mark_edges(h->next());
+        mark_edges(h->next()->next());
+        mark_edges(h->opposite()->next());
+        mark_edges(h->opposite()->next()->next());
       }
 	}
 }
@@ -49,11 +67,11 @@ bool is_everythig_optimal(Suspect_Map& suspects)
   return x == 1;
 }
 
+
 int main(int argc, char** argv)
 {
   lop<double>();
   //map to tag the suspects
-  Suspect_Map suspects;
   Triangulation tri(std::cin);
   Kernel::Vector_2 u(1,0);
   Kernel::Vector_2 v(1,1);
@@ -72,9 +90,12 @@ int main(int argc, char** argv)
 		Triangulation::Vertex_const_handle a = halfedgeIter->vertex();
 		Triangulation::Vertex_const_handle b =   
 		halfedgeIter->opposite()->vertex();
-    suspects[halfedgeIter] = 0;
+    if(is_flippable(halfedgeIter))
+    {
+      suspects[halfedgeIter] = 0;
+    }
 	}
-
+  
     // for each edge
   while(!is_everythig_optimal(suspects))
   {
@@ -83,25 +104,35 @@ int main(int argc, char** argv)
       Triangulation::Vertex_const_handle b =   
       halfedgeIter->opposite()->vertex();
 
-      //if not preferred direction localy delauny,
-      auto pa = halfedgeIter->opposite()->next()->vertex()->point();
-      auto pb = halfedgeIter->vertex()->point();
-      auto pc = halfedgeIter->opposite()->vertex()->point();
-      auto pd = halfedgeIter->next()->vertex()->point();
-      if(k.is_locally_pd_delaunay_edge(pa,pb,pc,pd,u,v))
+      // //if not preferred direction localy delauny,
+      // auto pa = halfedgeIter->opposite()->next()->vertex()->point();
+      // auto pb = halfedgeIter->vertex()->point();
+      // auto pc = halfedgeIter->opposite()->vertex()->point();
+      // auto pd = halfedgeIter->next()->vertex()->point();
+
+      
+      auto pd = halfedgeIter->opposite()->next()->vertex()->point();
+      auto pa = halfedgeIter->vertex()->point();
+      auto pb = halfedgeIter->opposite()->vertex()->point();
+      auto pc = halfedgeIter->next()->vertex()->point();
+      
+      if(k.is_locally_pd_delaunay_edge(pa,pb,pc,pd,u,v) && is_flippable(halfedgeIter))
       {
+        std::cout << "faces in You are inside:\n";
+        
           tri.flip_edge(halfedgeIter);
           suspects[halfedgeIter] = 1;
-          mark_potential_suspects(tri,halfedgeIter,suspects);
+          suspects[halfedgeIter->opposite()] = 1;
+          mark_potential_suspects(tri,halfedgeIter);
       }
       else
       {
-          suspects[halfedgeIter] = 1;          
+          suspects[halfedgeIter] = 1;
+          suspects[halfedgeIter->opposite()] = 1;                  
       }
     }
   } 
   
-
 	// Output the triangulation in OFF format to standard output.
 	std::cout << "Triangulation in OFF format:\n";
 	tri.output_off(std::cout);
